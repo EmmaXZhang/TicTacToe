467c4afebf29d20f195b69cdc733438e0689aa00
EmmaZhang <emmazxd@gmail.com>
2024-02-02 20:54:51 +0800

    add placeMark function

--- TTT.js
+++ TTT.js
@@ -11,9 +11,41 @@ const winningCombination = [
 ];
 
 // state varibles
+const state = {
+  currentClass: "",
+  circleClass: "circle",
+  xClass: "x",
+  circleTurn: "",
+};
 
 //cached elements
+const elements = {
+  cells: document.querySelectorAll(".cell"),
+};
 
 //event listener
+elements.cells.forEach(function (cell) {
+  cell.addEventListener("click", handleClick, { once: true }); //only can click one time
+});
 
 //function
+function handleClick(event) {
+  //define clicked cell
+  const cell = event.target;
+  //add X or O class to show mark
+  state.currentClass = state.circleTurn ? state.circleClass : state.xClass; // initial show "X" class
+  placeMark(cell, state.currentClass);
+
+  //check wins
+  //check draw
+
+  //switch player
+  switchPlayer(cell, state.currentClass);
+}
+
+//choose placeMark
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+//switch player
