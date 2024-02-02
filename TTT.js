1962a673c6f771d5637dae9aaf3b481644ea0f6e
EmmaZhang <emmazxd@gmail.com>
2024-02-02 19:49:14 +0800

    html,css final code commit

---
+++ TTT.js
@@ -0,0 +1,19 @@
+// const
+const winningCombination = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+// state varibles
+
+//cached elements
+
+//event listener
+
+//function
